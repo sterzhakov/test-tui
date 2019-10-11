27cfa7f566cc0ff137bfdc9c8adeba569d4ac42a
Igor <sterjakov@icloud.com>
2019-10-11 17:13:25 +0300

    Increase test data count

--- src/pages/Hotels/helpers/fetchData.js
+++ src/pages/Hotels/helpers/fetchData.js
@@ -2,7 +2,7 @@ import range from '../utils/range';
 import sleep from '../utils/sleep';
 import generateHotel from './generateHotel';
 
-const hotels = range(15).map(generateHotel);
+const hotels = range(45).map(generateHotel);
 
 const fetchData = async (props) => {
   const { limit, offset } = props;
